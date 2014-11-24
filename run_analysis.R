4e4f512cab88bae4dac233ede662d71b8c9b61c2
carlospiad <carlospiad@gmail.com>
2014-11-23 19:54:19 -0500

    add description to CodeBook and readme

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,33 @@
-## Check for data.table package and install if necesary
+## Install data.table package first
 
-if (!require("data.table")) {
-  install.packages("data.table")
-}
+
+
+# Create variables and load labels into R
+activity_labels <- read.table('dataset/activity_labels.txt')
+features <- read.table('dataset/features.txt')
+train_labels <- read.table('dataset/train/y_train.txt')
+test_labels <- read.table('dataset/test/y_test.txt')
+
+
+#Create variables and load data sets
+train_set <- read.table('dataset/train/X_train.txt')
+test_set <- read.table('dataset/test/X_test.txt')
+
+#Merge the training and test set
+
+all_set <- merge(train_set,test_set)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
